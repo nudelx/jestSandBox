5b71fb2910905657aff66d908692a2d3004fe2ae
nudelx <alex.nudelman@samanage.com>
2017-05-27 16:17:05 +0300

    added nav bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './style/App.css';
 import Header from './componets/header'
 import PageContent from './componets/pageContent'
+import Nav from './componets/navigator.js'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Nav />
       <PageContent />
     </div>
   );
---
+++ src/componets/navigator.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../style/App.css';
+const Nav = () => {
+  return (
+    <div className='nav-bar'>
+      <ul>
+        <li>Page 1</li>
+        <li>Page 2</li>
+        <li>Page 3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Nav
