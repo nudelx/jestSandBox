cdd3ae821c343e06efe4b5fb6eae7025da312ff2
nudelx <alex.nudelman@samanage.com>
2017-05-28 17:30:11 +0300

    jest done

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
-//
-//
-// describe('this is main app test', () => {
-//   it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<App />, div);
-//   });
-// })
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/App.js';
+
+
+describe('this is main app test', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
+})
--- __tests__/logo.spec.js
+++ __tests__/logo.spec.js
@@ -2,9 +2,21 @@ import React from  'react'
 import { shallow } from 'enzyme'
 import Logo from '../src/componets/logo'
 
-describe('logo', () => {
-  it('loogo test', () => {
-    const componet = shallow(<Logo />)
-    console.log(componet)
+describe('full logo test', () => {
+  it('should include div with logo class', () => {
+    const component = shallow(<Logo />)
+    expect(component.containsMatchingElement(
+      <div className="sw-logo"></div>
+    )).toEqual(true);
+  })
+
+  it('should have only one div ', () => {
+    const component = shallow(<Logo />)
+    expect(component.children().length).toBe(0)
+  })
+
+  it('shouldn\'t have more props', () => {
+    const component = shallow(<Logo />)
+    expect(component.props()).toEqual({"className": "sw-logo"})
   })
 })
