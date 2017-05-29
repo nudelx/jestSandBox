4d0274d64507182d865e51baa007a29da89f7473
nudelx <alex.nudelman@samanage.com>
2017-05-29 17:21:04 +0300

    stash

--- __tests__/movieActionsCreator.spec.js
+++ __tests__/movieActionsCreator.spec.js
@@ -1,7 +1,7 @@
 import { stateProcessor } from '../src/redux/reducer/helpers/movieStateProcessor'
 import  { MovieFetching, MovieFetchDone, MovieFetchError } from  '../src/redux/action/actionsType'
 import enzymeSerializer from 'enzyme-to-json/serializer';
-import MovieActionsCreator from '../src/redux/action/movieActionCreator'
+import { MovieActionsCreator,  tryGetMovie }  from '../src/redux/action/movieActionCreator'
 
 const page  = 'page_1'
 const movie = { movie: 'test movie '}
@@ -19,9 +19,27 @@ describe('it create actions propertly', () => {
     const state = MovieActionsCreator.getMovieDone(movie,page)
     expect(state).toMatchSnapshot()
   })
+
   it('getMovieError', () => {
     expect.addSnapshotSerializer(enzymeSerializer)
     const state = MovieActionsCreator.getMovieError(error)
     expect(state).toMatchSnapshot()
   })
+
+
+})
+
+
+describe('it should try to get a movie', () => {
+
+  it('getMovieError', () => {
+    expect.addSnapshotSerializer(enzymeSerializer)
+    tryGetMovie(1).then(d => d.json()).then(d => {
+      console.log(d)
+      expect(1).toBe(1)
+    })
+
+  })
+
+
 })
