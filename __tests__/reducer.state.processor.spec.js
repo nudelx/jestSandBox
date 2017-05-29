f59694bf70be66bb088a9b0de7c47c8a6cc22b65
nudelx <alex.nudelman@samanage.com>
2017-05-29 14:18:26 +0300

    added test for state processor

--- __tests__/reducer.state.processor.spec.js
+++ __tests__/reducer.state.processor.spec.js
@@ -1,11 +1,26 @@
 import { stateProcessor } from '../src/redux/reducer/helpers/movieStateProcessor'
-jest.mock('../src/redux/reducer/helpers/movieStateProcessor',function () {
-  console.log(arguments)
-});
+import  { MovieFetching, MovieFetchDone, MovieFetchError } from  '../src/redux/action/actionsType'
+import enzymeSerializer from 'enzyme-to-json/serializer';
 
-describe('it should mock and call movieStateProcessor', () => {
-  it('dssssss', () => {
-    console.log(stateProcessor)
-    expect(true).toBe(true)
+const actionObject = { error: 'bla bla bla ' , page:'test_1', movie: {test_movie: 'test_movie'} }
+
+describe('it should process the state', () => {
+
+  it('MovieFetching', () => {
+    expect.addSnapshotSerializer(enzymeSerializer)
+    const state = stateProcessor[MovieFetching]({}, actionObject)
+    expect(state).toMatchSnapshot()
+  })
+
+  it('MovieFetchDone', () => {
+    expect.addSnapshotSerializer(enzymeSerializer)
+    const state = stateProcessor[MovieFetchDone]({}, actionObject)
+    expect(state).toMatchSnapshot()
+  })
+
+  it('MovieFetchError', () => {
+    expect.addSnapshotSerializer(enzymeSerializer)
+    const state = stateProcessor[MovieFetchError]({}, actionObject)
+    expect(state).toMatchSnapshot()
   })
 })
