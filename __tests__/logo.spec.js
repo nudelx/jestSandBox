6e76c53c58e41df4a8797b94ea4b270c56b23823
nudelx <alex.nudelman@samanage.com>
2017-05-29 10:36:49 +0300

    snapshots + coverage

--- __tests__/logo.spec.js
+++ __tests__/logo.spec.js
@@ -1,6 +1,15 @@
 import React from  'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 import Logo from '../src/componets/logo'
+import toJson from 'enzyme-to-json';
+import enzymeSerializer from 'enzyme-to-json/serializer';
+import objectToJson from 'object-to-json';
+
+// TODO functions snapshot functions
+// TODO custom functions snapshot
+// TODO component snapshot
+// TODO mock
+
 
 describe('full logo test', () => {
   it('should include div with logo class', () => {
@@ -18,5 +27,16 @@ describe('full logo test', () => {
   it('shouldn\'t have more props', () => {
     const component = shallow(<Logo />)
     expect(component.props()).toEqual({"className": "sw-logo"})
+  })
+
+  it('should match the dom presentation snapshot', () => {
+    expect.addSnapshotSerializer(enzymeSerializer)
+    const component = mount(<Logo />)
+    expect(component).toMatchSnapshot()
+  })
+
+  it('should match the dom presentation snapshot Ω ', () => {
+    const component = mount(<Logo />)
+    expect(toJson(component)).toMatchSnapshot()
   })
 })
