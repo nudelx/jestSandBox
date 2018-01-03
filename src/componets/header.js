62f283b39385644f80ce69b49cfdb67fd29ff995
Alex Nudelman <nudelx@gmail.com>
2018-01-03 23:09:36 +0200

    d

--- src/componets/header.js
+++ src/componets/header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import logo from '../logo.svg';
 import '../style/App.css';
+import WithTrailerPreview from '../HOC/WithTrailerPreview'
+
+const Test = () => <div></div>
+
+
+const AA = WithTrailerPreview(Test)
 
 const Header = ({ txt }) => {
   txt = txt || 'Welcome to React'
@@ -8,6 +14,7 @@ const Header = ({ txt }) => {
     <div className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h2>{txt}</h2>
+      <AA/>
     </div>
   )
 }
