bd31db4b487913e18e04d2d60bde6799921b0f7d
Alex Nudelman <nudelx@gmail.com>
2018-11-20 21:26:03 +0200

    done

--- __tests__/components/nav.spec.js
+++ __tests__/components/nav.spec.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import Enzyme, { shallow, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import Nav from '../../src/components/navigator'
-import toJson from 'enzyme-to-json'
+import React from "react"
+import Enzyme, { shallow } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import Nav from "../../src/components/navigator"
+import toJson from "enzyme-to-json"
 Enzyme.configure({ adapter: new Adapter() })
 const onClick = jest.fn()
 let component = null
 
-describe('Navigator component ', () => {
+describe("Navigator component ", () => {
   beforeEach(() => {
     component = shallow(<Nav onClick={onClick} />)
   })
 
-  it('should render without crashing', () => {
+  it("should render without crashing", () => {
     expect(toJson(component)).toMatchSnapshot()
   })
 
-  it('should have buttons with click function', () => {
-    expect(toJson(component.find('li'))).toMatchSnapshot()
+  it("should have buttons with click function", () => {
+    expect(toJson(component.find("li"))).toMatchSnapshot()
   })
 
-  it('should pass the params to the click function', () => {
-    component.find('li').map(item => item.simulate('click'))
+  it("should pass the params to the click function", () => {
+    component.find("li").map(item => item.simulate("click"))
     expect(onClick.mock.calls).toMatchSnapshot()
   })
 })
--- __tests__/containers/navigatorConteiner.spec.js
+++ __tests__/containers/navigatorConteiner.spec.js
@@ -1,38 +1,38 @@
-import React from 'react'
-import Enzyme, { shallow, mount } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import NavigatorContainer from '../../src/containers/navigatorContainer'
-import toJson from 'enzyme-to-json'
-import configureStore from 'redux-mock-store'
-import Nav from '../../src/components/navigator'
+import React from "react"
+import Enzyme, { shallow, mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import NavigatorContainer from "../../src/containers/navigatorContainer"
+import toJson from "enzyme-to-json"
+import configureStore from "redux-mock-store"
+import Nav from "../../src/components/navigator"
 Enzyme.configure({ adapter: new Adapter() })
 
-const initialState = { content: 'test_1', movies: { movie: 'test' } }
+const initialState = { content: "test_1", movies: { movie: "test" } }
 const mockStore = configureStore()
 let store = mockStore(initialState)
 let container = null
 
-describe('NavigatorContainer test ', () => {
+describe("NavigatorContainer test ", () => {
   beforeEach(() => {
     store = mockStore(initialState)
     store.dispatch = jest.fn()
     container = mount(<NavigatorContainer store={store} />)
   })
 
-  it('should render without crashing with Nav', () => {
+  it("should render without crashing with Nav", () => {
     expect(toJson(container)).toMatchSnapshot()
   })
 
-  it('shold render navigator', () => {
+  it("should run redux process navigator", () => {
     expect(toJson(container)).toMatchSnapshot()
     const nav = container.find(Nav)
-    nav.find('li').map(item => item.simulate('click'))
+    nav.find("li").map(item => item.simulate("click"))
     expect(store.dispatch.mock.calls).toMatchSnapshot()
   })
 
-  it('shold render dispatch double action ', () => {
+  it("should render dispatch double action ", () => {
     const nav = container.find(Nav)
-    nav.find('li').map(item => item.simulate('click'))
+    nav.find("li").map(item => item.simulate("click"))
     expect(store.dispatch.mock.calls).toMatchSnapshot()
   })
 })
