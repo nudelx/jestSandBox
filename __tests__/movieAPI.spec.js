31b014eaf734857ca6e45c3e3a23369e4635efac
Alex Nudelman <nudelx@gmail.com>
2018-11-24 14:06:02 +0200

    ficed circular

--- __tests__/movieAPI.spec.js
+++ __tests__/movieAPI.spec.js
@@ -1,31 +1,31 @@
-import MovieAPI from '../src/helper/MovieAPI'
-jest.mock('../src/helper/MovieAPI')
+import MovieAPI from "../src/helper/MovieAPI"
+jest.mock("../src/helper/MovieAPI")
 
-describe('testing movie API ', () => {
-  it('should return data object with title', () => {
+describe("testing movie API ", () => {
+  it("should return data object with title", () => {
     expect.assertions(1)
-    return MovieAPI.fetch('bla/bla/bla/1').then(data =>
-      expect(data.title).toEqual('Hope it Will work')
+    return MovieAPI.fetch("bla/bla/bla/1").then(data =>
+      expect(data.title).toEqual("Hope it Will work")
     )
   })
 
-  it('should return data object with title', () => {
+  it("should return data object with title", () => {
     expect.assertions(1)
-    return MovieAPI.fetch('bla/bla/bla/1').then(data =>
-      expect(data.director).toEqual('BB-8')
-    )
+    return MovieAPI.fetch("bla/bla/bla/1").then(data => {
+      expect(data.director).toEqual("BB-8")
+    })
   })
 
-  it('should match the snapshot ', () => {
+  it("should match the snapshot ", () => {
     expect.assertions(1)
-    return MovieAPI.fetch('bla/bla/bla/1').then(data =>
+    return MovieAPI.fetch("bla/bla/bla/1").then(data =>
       expect(data).toMatchSnapshot()
     )
   })
 
-  it('should match the error', () => {
+  it("should match the error", () => {
     expect.assertions(1)
-    return MovieAPI.fetch('bla/bla/bla/5')
+    return MovieAPI.fetch("bla/bla/bla/5")
       .then(data => expect(data).toMatchSnapshot())
       .catch(error => expect(error).toMatchSnapshot())
   })
