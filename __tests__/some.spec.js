ff3366b98fa3e61ee376f70c93c7683b9be7541e
nudelx <nudelx@gmail.com>
2018-11-26 20:08:19 +0200

    tmnp

--- __tests__/some.spec.js
+++ __tests__/some.spec.js
@@ -1,10 +1,31 @@
+import M from '../src/utilities/myModule'
 jest.mock('../src/utilities/myModule')
 
-import M from '../src/utilities/myModule'
-
 describe('bla', () => {
-  it('ddddd',() => {
+  it('ddddd', () => {
     M()
-    expect(1).toBe(1);
+    expect(1).toBe(1)
   })
 })
+
+// const mockFn = jest.mock()
+// const a = new mockFn()
+// const b = new mockFn()
+// const c = b.bind(a, 'test')
+
+// /***/
+
+// jest.mock('./banana')
+// import banana from './banana'
+// banana.getColor()
+// // banana.getColor.mock.calls
+// // lastCalledWith ?
+
+// /***/
+
+// jest.mock('./banana', () => ({
+//   getColor: jest.fn().mockImplementation(() => 42)
+// }))
+
+// import banana from './banana'
+// banana.getColor()  //42
