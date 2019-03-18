e197d63ed82210a21366a0109035fb53d0b5582e
nudelx <nudelx@gmail.com>
2019-03-18 14:10:16 +0200

    added all

--- __tests__/test.pubsub.spec.js
+++ __tests__/test.pubsub.spec.js
@@ -1,11 +1,12 @@
-import EventSys from '../hadar/pubsub' /// here you say that it should use the mock !!!!
-
+import EventSys from '../hadar/pubsub'
 import ProductGallery from '../hadar/someComponent'
 import React from 'react'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
+/// here you say that it should use the mock !!!!
 jest.mock('../hadar/pubsub')
+
 Enzyme.configure({ adapter: new Adapter() })
 
 describe('mock test', () => {
